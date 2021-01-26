2ad66f8f3033c756d5cadffe21eccb984a4711ff
Cletche Nafantchamna <cletche1991@gmail.com>
2021-01-26 02:11:21 -0300

    console.ReadKey

--- POO/POO/Service/ContaCorrenteService.cs
+++ POO/POO/Service/ContaCorrenteService.cs
@@ -10,6 +10,7 @@ namespace POO.Service
             Console.WriteLine("Fvaor digite o seu nome: " + "\n");
             var nome = Console.ReadLine();
             Console.WriteLine("Informe valor que deseja sacar: " + "\n");
+            Console.ReadKey();
         }
         public void OperacaoDeposito()
         {
@@ -17,12 +18,14 @@ namespace POO.Service
             Console.WriteLine("Favor digite o seu nome: " + "\n");
             var nome = Console.ReadLine();
             Console.WriteLine("Informe valor que deseja depositar: " + "\n");
+            Console.ReadKey();
         }
         public void OperacaoTransferencia()
         {
             Console.WriteLine("Favor digite o seu nome: " + "\n");
             var nome = Console.ReadLine();
             Console.WriteLine("Informe valor que deseja transferir: " + "\n");
+            Console.ReadKey();
         }
     }
 }
