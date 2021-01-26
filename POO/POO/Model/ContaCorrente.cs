7d983f7e09fdc64c71cd80b8a412df46bcfa1f4d
Cletche Nafantchamna <cletche1991@gmail.com>
2021-01-26 01:47:17 -0300

    Criando a clasee ContaCorrente

---
+++ POO/POO/Model/ContaCorrente.cs
@@ -0,0 +1,11 @@
+﻿namespace POO.Model
+{
+    public class ContaCorrente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
